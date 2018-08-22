d2c721d53b5d4d24a0c1a5bc34308f9f6bff3f33
Hugo Daniel <hugodaniel.icam@gmail.com>
2018-08-22 15:43:44 +0200

    Use postgresql instead of runner

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ function set_gemfile(){
 echo "Setup Spree defaults and creating test application..."
 rm_gemfile_lock
 bundle check || bundle install
-DB=runner bundle exec rake test_app
+DB=postgres bundle exec rake test_app
 
 # Spree API
 echo "Setup Spree API and running RSpec..."
